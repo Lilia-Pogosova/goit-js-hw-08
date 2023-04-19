755917e82d5e64b647adcf36e65b221d07089939
Lilia <Liliapogosova@gmail.com>
2023-04-19 18:40:42 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,57 +1,34 @@
 // Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Change code below this line
+import { galleryItems } from './gallery-items';
 
+
+// Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-galleryContainer.addEventListener('click', onImgClick);
 
 
 function createGalleryItemsMarkup(items) {
-    return items
-        .map(({ preview, original, description }) => {
-            return `<li class="gallery__item">
-<a class="gallery__link" href="${original}">
-<img
+    return items.map(({ preview, original, description }) => {
+        return `
+<a class="gallery__item" href="${original}">
+    <img
     class="gallery__image"
     src="${preview}"
-    data-source="${original}"
     alt="${description}"
     />
 </a>
-</li>`;
-        })
-        .join("");
+`;
+    }).join('');
 }
 
-const instance = basicLightbox.create(
-    `<img width="1280" height="auto" src="">`,
-    {
-        onShow: (instance) => {
-            window.addEventListener('keydown', onEscKeyPress);
-        },
-        onClose: (instance) => {
-            window.removeEventListener('keydown', onEscKeyPress);
-        },
-    }
-);
 
-function onImgClick(evt) {
-    evt.preventDefault();
-    const datasetSource = evt.target.dataset.source;
-    if (!datasetSource) return;
-    instance.element().querySelector('img').src = datasetSource;
-    instance.show();
-}
-
-function onEscKeyPress(evt) {
-    if (evt.code !== 'Escape') return;
-    instance.close();
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
 
 
